e1857ba8523de46e40b486a639cccbc34890cefa
Shlomi Fish <shlomif@shlomifish.org>
2019-06-24 21:30:53 +0300

    appveyor fix #4 - python

--- fortune-mod/tests/fortune-m-test.py
+++ fortune-mod/tests/fortune-m-test.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
+import glob
 import os
 import subprocess
-inst_dir = os.getcwd() + "/fortune-m-INST_DIR"
+from os.path import join
+inst_dir = join(os.getcwd(), "fortune-m-INST_DIR")
 
-subprocess.call([inst_dir + "/games/fortune", "-m", "giants"])
+subprocess.check_call([
+    glob.glob(join(inst_dir, "games", "fortune")+'*')[0], "-m", "giants"])
