46d5231a62dbf9acdb3a14b47d845a43cfced676
Shlomi Fish <shlomif@shlomifish.org>
2019-12-18 13:14:45 +0200

    switch to rinutils cmake/pkgconf package

--- CI-testing/continuous-integration-testing.pl
+++ CI-testing/continuous-integration-testing.pl
@@ -21,6 +21,7 @@ sub do_system
 my $IS_WIN = ( $^O eq "MSWin32" );
 my $SEP    = $IS_WIN ? "\\" : '/';
 my $MAKE   = $IS_WIN ? 'gmake' : 'make';
+my $SUDO   = $IS_WIN ? '' : 'sudo';
 
 my $cmake_gen;
 GetOptions( 'gen=s' => \$cmake_gen, )
@@ -33,13 +34,54 @@ if ( defined $cmake_gen )
 }
 mkdir('B');
 chdir('B');
+if ( !$ENV{SKIP_RINUTILS_INSTALL} )
+{
+    do_system(
+        {
+            cmd => [ qw#git clone https://github.com/shlomif/rinutils#, ]
+        }
+    );
+    do_system(
+        {
+            cmd => [
+                      qq#cd rinutils && mkdir B && cd B && cmake #
+                    . ( defined($cmake_gen) ? qq# -G "$cmake_gen" # : "" )
+                    . (
+                    defined( $ENV{CMAKE_MAKE_PROGRAM} )
+                    ? " -DCMAKE_MAKE_PROGRAM=$ENV{CMAKE_MAKE_PROGRAM} "
+                    : ""
+                    )
+                    . ( $IS_WIN ? " -DCMAKE_INSTALL_PREFIX=C:/foo " : '' )
+                    . qq# .. && $SUDO $MAKE install#
+            ]
+        }
+    );
+}
+do_system( { cmd => [ "cmake", "--version" ] } );
+my $CMAKE_MODULE_PATH = join ";",
+    (
+    map { ; $_, s%/%\\%gr } (
+        map { ; $_, "$_/Rinutils", "$_/Rinutils/cmake" }
+            map { ; $_, "$_/lib", "$_/lib64" } ( map { ; "c:$_", $_ } ("/foo") )
+    )
+    );
+
+# die "<$CMAKE_MODULE_PATH>";
+if ($IS_WIN)
+{
+    ( $ENV{CMAKE_MODULE_PATH} //= '' ) .= ";$CMAKE_MODULE_PATH;";
+
+    # ( $ENV{PKG_CONFIG_PATH} //= '' ) .= ";C:\\foo\\lib\\pkgconfig;";
+    ( $ENV{PKG_CONFIG_PATH} //= '' ) .=
+        ";/foo/lib/pkgconfig/;/c/foo/lib/pkgconfig/";
+    $ENV{RINUTILS_INCLUDE_DIR} = "C:/foo/include";
+}
 do_system(
     {
         cmd => [
-            $^X, "..${SEP}scripts${SEP}Tatzer",
-            ( defined($cmake_gen) ? (qq#--gen="$cmake_gen"#) : () )
+                  "mkdir B && cd B && cmake  "
+                . ( defined($cmake_gen) ? qq#-G "$cmake_gen"# : "" )
+                . " ../fortune-mod && $MAKE && $MAKE check"
         ]
     }
 );
-do_system( { cmd => [$MAKE] } );
-do_system( { cmd => [ $MAKE, "check", ] } );
