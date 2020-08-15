350cd84dbb775151af556dc998884a50bc8c5150
Shlomi Fish <shlomif@shlomifish.org>
2020-08-15 18:44:15 +0300

    Convert more manpages to DocBook 5 XML.
    See:
    
    * https://github.com/shlomif/fortune-mod/issues/41
    
    Made use of doclifter ( http://www.catb.org/~esr/doclifter/ ),
    docmake ( https://www.shlomifish.org/open-source/projects/docmake/ ),
    cmake, and Perl 5. Thanks!
    
    The current plan is to provide the resultant nroff outputs (or easily 's/.../.../'able
    templates of them) inside the source tarballs to avoid a build time dependency on
    DocBook XSL and docmake.

---
+++ fortune-mod/fortune/gen-randstr-manpage.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use autodie;
+
+use Getopt::Long qw/ GetOptions /;
+use Path::Tiny qw/ path tempdir tempfile cwd /;
+
+sub do_system
+{
+    my ( $self, $args ) = @_;
+
+    my $cmd = $args->{cmd};
+    print "Running [@$cmd]\n";
+    if ( system(@$cmd) )
+    {
+        die "Running [@$cmd] failed!";
+    }
+
+    return;
+}
+
+my $output_fn;
+my $cookiedir;
+my $ocookiedir;
+my $no_offensive = 0;
+my $CMAKE_CURRENT_SOURCE_DIR;
+GetOptions(
+    '--src-dir=s'          => \$CMAKE_CURRENT_SOURCE_DIR,
+    '--cookiedir=s'        => \$cookiedir,
+    '--ocookiedir=s'       => \$ocookiedir,
+    '--without-offensive!' => \$no_offensive,
+    '--output=s'           => \$output_fn,
+) or die "Wrong options - $!";
+
+die "missing --src-dir" if ( not $CMAKE_CURRENT_SOURCE_DIR );
+__PACKAGE__->do_system(
+    {
+        cmd => [
+            "docmake", "manpages",
+            "${CMAKE_CURRENT_SOURCE_DIR}/util/randstr.docbook5.xml",
+        ],
+    },
+);
+
+path("${CMAKE_CURRENT_SOURCE_DIR}/util/randstr.man")
+    ->spew_utf8(
+    cwd()->child("randstr.1")->slurp_utf8() =~ s#^\s+(\.RE|\.PP)\s*$#$1#gmsr =~
+        s#^\s+$##gmsr );
