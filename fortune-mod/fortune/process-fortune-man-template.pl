d691c451ef2f03a5bd425ae374b3756c436d63d2
Shlomi Fish <shlomif@shlomifish.org>
2020-08-18 15:02:39 +0300

    Convert more manpages to DocBook 5 XML.
    post process the directory names.
    
    See:
    
    * https://github.com/shlomif/fortune-mod/issues/41
    
    Made use of doclifter ( http://www.catb.org/~esr/doclifter/ ),
    docmake ( https://www.shlomifish.org/open-source/projects/docmake/ ),
    cmake, and Perl 5. Thanks!
    
    The current plan is to provide the resultant nroff outputs (or easily 's/.../.../'able
    templates of them) inside the source tarballs to avoid a build time dependency on
    DocBook XSL and docmake.

---
+++ fortune-mod/fortune/process-fortune-man-template.pl
@@ -0,0 +1,73 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use autodie;
+
+use File::Basename qw / dirname /;
+use File::Path qw / mkpath /;
+use Getopt::Long qw/ GetOptions /;
+
+my $input_fn;
+my $output_fn;
+my $cookiedir;
+my $ocookiedir;
+my $no_offensive = 0;
+GetOptions(
+    '--cookiedir=s'        => \$cookiedir,
+    '--ocookiedir=s'       => \$ocookiedir,
+    '--without-offensive!' => \$no_offensive,
+    '--output=s'           => \$output_fn,
+    '--input=s'            => \$input_fn,
+) or die "Wrong options - $!";
+
+if ( !defined($input_fn) )
+{
+    die "Please specify --input";
+}
+if ( !defined($output_fn) )
+{
+    die "Please specify --output";
+}
+
+if ( !defined($cookiedir) )
+{
+    die "Please specify cookiedir";
+}
+
+my $OFF = ( !$no_offensive );
+
+if ( $OFF and !defined($ocookiedir) )
+{
+    die "Please specify ocookiedir";
+}
+
+my $dirname = dirname($output_fn);
+if ( $dirname and ( !-e $dirname ) )
+{
+    mkpath($dirname);
+}
+
+# The :raw is to prevent CRs on Win32/etc.
+open my $out, '>:encoding(utf-8)', $output_fn;
+my $text = _utf8_slurp($input_fn);
+
+sub _utf8_slurp
+{
+    my $filename = shift;
+
+    open my $in, '<:encoding(utf8)', $filename
+        or die "Cannot open '$filename' for slurping - $!";
+
+    local $/;
+    my $contents = <$in>;
+
+    close($in);
+
+    return $contents;
+}
+
+die if ( $text =~ s#\Q[[cookiedir_placeholder]]\E#${cookiedir}#gms ) > 1;
+die if ( $text =~ s#\Q[[ocookiedir_placeholder]]\E#${ocookiedir}#gms ) > 1;
+$out->print($text);
+close($out);
