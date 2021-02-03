5faee9d112d985d1964ccafcfd03203debb2c310
Shlomi Fish <shlomif@shlomifish.org>
2021-02-03 07:23:43 +0200

    Fix tidyall tests.
    See:
    
    https://metacpan.org/release/Code-TidyAll .

--- fortune-mod/util/strfile.h
+++ fortune-mod/util/strfile.h
@@ -50,8 +50,8 @@ typedef struct
     uint32_t str_longlen;  /* length of longest string */
     uint32_t str_shortlen; /* length of shortest string */
 #define STR_RANDOM 0x1     /* randomized pointers */
-#define STR_ORDERED 0x2 /* ordered pointers */
-#define STR_ROTATED 0x4 /* rot-13'd text */
+#define STR_ORDERED 0x2    /* ordered pointers */
+#define STR_ROTATED 0x4    /* rot-13'd text */
     uint32_t str_flags;    /* bit field for flags */
     uint8_t stuff[4];      /* long aligned space */
 #define str_delim stuff[0] /* delimiting character */
