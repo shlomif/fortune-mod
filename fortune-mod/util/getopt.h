eb7bbace5986815855212a852d60db83d79104eb
Shlomi Fish <shlomif@shlomifish.org>
2021-11-20 09:15:03 +0200

    refactoring

--- fortune-mod/util/getopt.h
+++ fortune-mod/util/getopt.h
@@ -1,15 +1,7 @@
-/*
- * getopt.h
- */
-
-#ifndef GETOPT_H
-#define GETOPT_H
-
+#pragma once
 extern int opterr,   /* if error message should be printed */
     optind,          /* index into parent argv vector */
     optopt,          /* character checked for validity */
     optreset;        /* reset getopt */
 extern char *optarg; /* argument associated with option */
 int getopt(int nargc, char *const nargv[], const char *ostr);
-
-#endif /* !GETOPT_H */
