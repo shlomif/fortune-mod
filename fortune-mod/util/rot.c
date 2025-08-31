e04427d43e4e5daa29cbe5121e0a11d1631fd136
Jimmy Cassis <KernelOops@outlook.com>
2025-08-30 21:20:50 -0700

    Fix functionality on Windows

--- fortune-mod/util/rot.c
+++ fortune-mod/util/rot.c
@@ -3,8 +3,19 @@
 #include <stdio.h>
 #include <ctype.h>
 
+#ifdef _WIN32
+#include <fcntl.h>
+#include <io.h>
+#endif
+
 int main(void)
 {
+#ifdef _WIN32
+    // Force binary mode to avoid CRLF translation.
+    setmode(_fileno(stdin), _O_BINARY);
+    setmode(_fileno(stdout), _O_BINARY);
+#endif
+
     int a;
     while ((a = getchar()) != EOF)
     {
