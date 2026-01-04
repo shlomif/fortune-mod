74c1cfa3a44e3de659eb79a0619e6c219617d647
Shlomi Fish <shlomif@shlomifish.org>
2026-01-04 12:43:52 +0200

    docker-based tests
    This is done to document build/tests dependencies:
    
    https://github.com/shlomif/fortune-mod/issues/121

---
+++ scripts/run-build-and-tests-inside-docker.pl
@@ -0,0 +1,126 @@
+#! /usr/bin/env perl
+
+use strict;
+use warnings;
+use 5.014;
+use autodie;
+
+use Path::Tiny qw/ path /;
+use Docker::CLI::Wrapper::Container v0.0.4 ();
+
+my $FEDORA_SYS_WITH_A_NEW_ENUF_pysol_cards = "fedora:43";
+my $SYS       = $FEDORA_SYS_WITH_A_NEW_ENUF_pysol_cards;
+my $CONTAINER = "fortunemod_buildproc_fedora";
+my $obj       = Docker::CLI::Wrapper::Container->new(
+    { container => $CONTAINER, sys => $SYS, }, );
+
+my @deps;
+@deps = (
+    qw/
+        cmake
+        cpanminus
+        docbook-dtds
+        docbook-style-xsl
+        docbook-utils
+        docbook5-schemas
+        docbook5-style-xsl
+        gcc
+        gcc-c++
+        git
+        glibc-devel
+        libcmocka-devel
+        libxslt-devel
+        make
+        perl-File-Find-Object
+        perl-Path-Tiny
+        perl-Test-Differences
+        perl-Test-RunValgrind
+        perl-Test-Trap
+        perl-autodie
+        perl-core
+        perl-devel
+        python3-pip
+        valgrind
+        /
+);
+
+if (0)
+{
+    @deps = map { /^BuildRequires:\s*(\S+)/ ? ("'$1'") : () }
+        path("freecell-solver.spec.in")->lines_utf8;
+}
+$obj->clean_up();
+$obj->run_docker();
+$obj->exe_bash_code( { code => "set -e -x ; mkdir -p /root/fortune-mod/", } );
+foreach my $suf ( "CI-testing", "scripts", "fortune-mod", )
+{
+    $obj->docker(
+        {
+            cmd => [ 'cp', "$suf", "${CONTAINER}:root/fortune-mod/$suf", ]
+        }
+    );
+}
+
+my $script = <<"EOSCRIPTTTTTTT";
+set -e -x
+cd ~/fortune-mod/fortune-mod
+sudo dnf -y upgrade --refresh
+sudo dnf -y install @deps
+sudo cpanm -v App::Docmake
+if false
+then
+    sudo pip3 install --prefix=/usr freecell_solver
+    pip3 install --user freecell_solver
+fi
+mkdir ../../b
+cd ../../b
+perl ../fortune-mod/scripts/Tatzer
+gmake VERBOSE=1
+gmake VERBOSE=1 check
+gmake VERBOSE=1 install
+# export HARNESS_VERBOSE=1
+# FCS_TEST_BUILD=1 perl ../run-tests.pl --execute="perl `pwd`/../t/t/build*.t"
+# FCS_TEST_BUILD=1 perl ../run-tests.pl --glob='build*.t'
+# FCS_TEST_BUILD=1 perl ../run-tests.pl --glob='build*.t'
+if false
+then
+    sudo dnf -y install clang clang-devel cpanminus perl-Code-TidyAll perl-LWP-Protocol-https nodejs nodejs-npm ocl-icd-devel opencl-headers pocl pocl-devel wget
+    sudo cpanm Docker::CLI::Wrapper::Container Text::Sprintf::Named Perl::Tidy
+    ( cd ../site/wml && bash bin/install-npm-deps.sh )
+fi
+EOSCRIPTTTTTTT
+
+$obj->exe_bash_code( { code => $script, } );
+$obj->clean_up();
+
+__END__
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2019 by Shlomi Fish
+
+This program is distributed under the MIT / Expat License:
+L<http://www.opensource.org/licenses/mit-license.php>
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+=cut
